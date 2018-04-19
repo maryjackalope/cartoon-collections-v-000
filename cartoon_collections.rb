8e1084a72fb9ebd7ff02ad77876756bd2530b388
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 18:48:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,11 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(array)
   i = 0 
-  while i < array.length
+  while i < array.length 
+  puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
+
 end
 
 def summon_captain_planet# code an argument here
