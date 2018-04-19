3e963d169b72d1c46c27670b245beb5f2042ce6a
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 19:02:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,8 @@ def long_planeteer_calls(array)
   end 
 end
 
+snacks = ["crackers", "gouda", "thyme"] 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
