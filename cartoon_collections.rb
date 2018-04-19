75350c447d64acd20687bb17f77ca1927b0ff35a
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 19:00:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,16 @@ def summon_captain_planet(array)
   new_array 
 end
 
-def long_planeteer_calls
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
